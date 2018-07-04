83515f045bce0bb3a7ac850192e16c2ae6f1bec5
Sean Hong <iamseanhong@gmail.com>
2018-07-04 15:33:47 -0700

    Open browser and newPage with puppeteer chromium

--- __test__/Header.test.js
+++ __test__/Header.test.js
@@ -1,3 +1,14 @@
+import puppeteer from 'puppeteer';
+
 it('1 + 2 = 3', () => {
   expect(1 + 2).toEqual(3);
 });
+
+it('puppeteer opens browser and page', async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  })
+
+  const page = await browser.newPage();
+})
+
