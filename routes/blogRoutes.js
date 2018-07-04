639c833a6c3aa02e1ee6c4396a426343eec0fc03
Sean Hong <iamseanhong@gmail.com>
2018-07-03 23:51:38 -0700

    Create uniq key for cache with getQuery & collection name

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -18,25 +18,6 @@ module.exports = app => {
   app.get('/api/blogs', requireLogin, async (req, res) => {
     const blogs = await Blog.find({ _user: req.user.id });
 
-    const redis = require('redis');
-    const redisUri = 'redis://127.00.1:6379';
-    const client = redis.createClient(redisUri);
-
-    client.get = util.promisify(client.get);
-
-    // Do we have any cached data in redis related to this query
-    const cachedBlogs = await client.get(req.user.id);
-
-    // if yes, then respond to the request right away from redis
-    if (cachedBlogs !== undefined && cachedBlogs !== null) {
-      console.log('SEND FROM REDIS');
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    // if no, store blogs to redis
-    client.set(req.user.id, JSON.stringify(blogs));
-
-    console.log('SEND FROM MONGODB');
     res.send(blogs);
   });
 
--- services/cache.js
+++ services/cache.js
@@ -4,7 +4,27 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
 // Have to use function() instead of =>
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = function() {
   console.log('ABOUT TO RUN A QUERY');
+
+  const key = JSON.stringify({ ...this.getQuery(), collection: mongoose.Collection.name });
+  console.log('key: ', key);
+
+
   return exec.apply(this, arguments);
-}
+};
+
+// const redis = require('redis');
+// const redisUri = 'redis://127.00.1:6379';
+// const client = redis.createClient(redisUri);
+// client.get = util.promisify(client.get);
+// // Do we have any cached data in redis related to this query
+// const cachedBlogs = await client.get(req.user.id);
+// // if yes, then respond to the request right away from redis
+// if (cachedBlogs !== undefined && cachedBlogs !== null) {
+//   console.log('SEND FROM REDIS');
+//   return res.send(JSON.parse(cachedBlogs));
+// }
+// // if no, store blogs to redis
+// client.set(req.user.id, JSON.stringify(blogs));
+// console.log('SEND FROM MONGODB');
