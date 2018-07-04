0c85ea670859c2fc38aa39cc9b4f3111075182e8
Sean Hong <iamseanhong@gmail.com>
2018-07-04 12:09:26 -0700

    Conver client. get set -> hget hset

--- services/cache.js
+++ services/cache.js
@@ -5,13 +5,15 @@ const util = require('util');
 const redisUri = 'redis://127.00.1:6379';
 const client = redis.createClient(redisUri);
 // promisify client.get
-client.get = util.promisify(client.get);
+// client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 // get reference to original mongoose exec func
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(option = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(option.key) || '';
   return this;
 };
 
@@ -24,24 +26,26 @@ mongoose.Query.prototype.exec = async function() {
   const key = JSON.stringify({ ...this.getQuery(), collection: mongoose.Collection.name });
 
   // see if the key exist in cache
-  const cachedDataStr = await client.get(key);
+  const cachedDataStr = await client.hget(this.hashKey, key);
+  console.log('cachedDataStr: ', cachedDataStr);
 
-  // if it does return that
+  // if cache data exist then return parsed data
   if (cachedDataStr) {
-    console.log('cachedDataStr: ', cachedDataStr);
+    console.log('Data form cahce');
     const parsedCachedData = JSON.parse(cachedDataStr);
 
-    Array.isArray(parsedCachedData)
-      ? parsedCachedData.map(obj => this.model(obj))
+    return Array.isArray(parsedCachedData)
+      ? parsedCachedData.map(obj => new this.model(obj))
       : new this.model(parsedCachedData);
-  }
+  } else {
+    console.log('Data form mongo');
+    // if cache does not exist then cache it then return data from mongo
+    const dataFromMongoDB = await exec.apply(this, arguments);
 
-  // else {
-  // else store it to redis then return data from mongo
-  const dataFromMongoDB = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(dataFromMongoDB));
-  return dataFromMongoDB;
-  // }
+    client.hset(this.hashKey, key, JSON.stringify(dataFromMongoDB), 'EX', 1);
+
+    return dataFromMongoDB;
+  }
 };
 
 // const redis = require('redis');
