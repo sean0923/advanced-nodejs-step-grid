d830e874a2cd96ff1bc3822f8a68b4e48cf7083d
Sean Hong <iamseanhong@gmail.com>
2018-07-17 21:11:34 -0700

    Replace page.close with browser.close at Proxy

--- __test__/helpers/CustomPage.js
+++ __test__/helpers/CustomPage.js
@@ -7,13 +7,23 @@ class CustomPage {
     });
 
     const page = await browser.newPage();
-    const customPage = new CustomPage(page);
+    const customPage = new CustomPage(page, browser);
 
     return new Proxy(customPage, {
       get: function(target, prop) {
+        // return customPage[prop] || browser[prop] || page[prop];
         return customPage[prop] || page[prop] || browser[prop];
       },
     });
+  }
+
+  constructor(page, browser) {
+    this.page = page;
+    this.browser = browser;
+  }
+
+  close() {
+    this.browser.close();
   }
 }
 
