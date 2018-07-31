2db1f17fe22bdc6512b4e8b7c594c3b0b1e3ebb2
Sean Hong <iamseanhong@gmail.com>
2018-07-30 21:07:46 -0700

    Clean up code

--- config/ci.js
+++ config/ci.js
@@ -1,10 +1,10 @@
 module.exports = {
   googleClientID: '426259059562-rgq3p6dp7e080f39p36trhjm4e4nusrf.apps.googleusercontent.com',
   googleClientSecret: 'glIilmxZh_EWGi8K4c_eF7Dc',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   session: 'eyJwYXNzcG9ydCI6eyJ1c2VyIjoiNWIzODQyOWM5NzVmOGYzNmVlYzc1ZTBiIn19',
   sessionSig: 'F0t1HI_ve5U2csNIP-vGd8EniRU',
   mLabUserId: '5b38429c975f8f36eec75e0b',
   redisUrl: 'redis://127.0.0.1:6379',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
 };
