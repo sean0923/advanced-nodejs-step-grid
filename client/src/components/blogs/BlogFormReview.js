51cabd3858e4a31a0df474e0ce23c7f0aec9dda9
Sean Hong <iamseanhong@gmail.com>
2018-07-30 22:01:07 -0700

    Add img file upload btn, got aws accessKeyId n secretAccessKey at dev.js

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -41,8 +43,12 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
   }
+
+  handleOnChange = e => {
+    this.setState({ file: e.target.files[0] });
+  };
 
   render() {
     return (
@@ -51,7 +57,7 @@ class BlogFormReview extends Component {
         {this.renderFields()}
 
         <h5>Add An Image</h5>
-        <input type="file" accept="image/*" />
+        <input type="file" accept="image/*" onChange={this.handleOnChange} />
 
         {this.renderButtons()}
       </form>
