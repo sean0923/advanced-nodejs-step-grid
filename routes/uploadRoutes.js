b3c6182ba1e74983acc4c8912fc653dd20af25a3
Sean Hong <iamseanhong@gmail.com>
2018-08-01 22:18:08 -0700

    Implement s3.getSignedUrl and get url

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,10 +1,26 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
+const uuidv1 = require('uuid/v1');
 
+const requireLogin = require('../middlewares/requireLogin');
 
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey,
+});
 
 module.exports = app => {
-  app.get('api/upload', (req, res) => {
-    
-  })
-}
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuidv1()}.jpeg}`;
+
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: 's3-testing-1234',
+        ContentType: 'jpeg',
+        Key: key,
+      },
+      (err, url) => res.send({ key, url })
+    );
+  });
+};
